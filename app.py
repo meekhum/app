1997be280c774ac21bde487d0bb1a503562a821b
meekhum <masteraanime818@gmail.com>
2025-02-25 14:09:34 +1000

    exp7

--- app.py
+++ app.py
@@ -10,11 +10,21 @@ with st.container():
     st.write("[Cute video of miku >](https://youtube.com/shorts/lTGmxLbg7Xo?si=IuiKk5FRRgJZls6c)")
     st.write("[Cute video of horse girl >](https://www.youtube.com/shorts/l74lS0jHnxo)")
 
+def replace_words(text):
+    replacements = {
+        "I": "you",
+        "am": "are"
+    }
+    for word, replacement in replacements.items():
+        text = text.replace(word, replacement)
+    return text
+    
 with st.container():
     a=st.text_input('Give your name')
     if a.strip():  
         st.write(f"What are you doing, {a}? How is mom?")
-        b=st.text_input('')
+        b = st.text_input('')
         if b.strip():
-            st.write(f"So {b}. Then what about dad?")
-            c=st.text_input()
+            b_modified = replace_words(b)
+            st.write(f"So {b_modified}. Then what about dad?")
+            c=st.text_input('')
