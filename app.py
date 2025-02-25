4ddb2747afea8e6649db6410ef8f486fddddc087
meekhum <masteraanime818@gmail.com>
2025-02-25 13:29:06 +1000

    exp1

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import streamlit as st
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
+with st.container():
+    st.subheader("Hi i am  :wave:")
+    st.title("a stupid prick")
+    st.write("I am wasting my time doing this and i dont know why i am doing this but it is kinda fun")
+    st.write("[Cute video of miku >](https://youtube.com/shorts/lTGmxLbg7Xo?si=IuiKk5FRRgJZls6c)")
+    st.write("[Cute video of horse girl >](https://www.youtube.com/shorts/l74lS0jHnxo)")
 
-st.subheader("Hi i am meekhum :wave:")
-st.title("a stupid prick")
-st.write("I am wasting my time doing this and i dont know why i am doing this but it is kinda fun")
-st.write("[Cute video of miku >](https://youtube.com/shorts/lTGmxLbg7Xo?si=IuiKk5FRRgJZls6c)")
-st.write("[Cute video of horse girl >](https://www.youtube.com/shorts/l74lS0jHnxo)")
+with st.container():
+    print("Hi")
